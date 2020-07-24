4ef232c84344d35ad5741d2d0a1d8242624ef1dd
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-07-24 22:31:44 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 var buttons=document.getElementsByClassName('button');
 var display=document.getElementById('displaytext');
+var current=document.getElementById('history');
 
 var operator=null;
 var operand1=0;
 var operand2=null;
 display.innerText=0;
+// current.innerText=0;
 
 for (var i = 0; i<buttons.length ; i++) {
 	buttons[i].addEventListener('click' , function(){ 
@@ -14,37 +16,47 @@ for (var i = 0; i<buttons.length ; i++) {
 		if(val=='C'){
 
 			display.innerText ="0" ;
-			operand1=null;
-			operand2=null;
-			operator=null;
+			current.innerText=" ";
 
 		}else if(val=='='){
 
 			operand2=parseFloat(displaytext.textContent) ;
-			var result=eval(operand1 + " " + operator + " " + operand2) ;
+
+			if(operator =='/' && operand2 == '0')
+           {
+              var result = "error";
+           }else{
+			result=eval(operand1 + " " + operator + "" + operand2) ;
+           }
+
 			displaytext.innerText=result;
+			current.innerText = current.textContent + "" + val + result;
 
 		}else if(val=='+'){
 
 			operator='+';
 			operand1=parseFloat(displaytext.textContent);
+			current.innerText = current.textContent + "" + val;
 			display.innerText =" " ;
 
 		}else if(val=='-'){
 
 			operator='-';
 			operand1=parseFloat(displaytext.textContent);
+			current.innerText = current.textContent + "" + val;
 			display.innerText =" " ;
 
 		}else if(val=='*'){
 			operator='*';
 			operand1=parseFloat(displaytext.textContent);
+			current.innerText = current.textContent + "" + val;
 			display.innerText =" " ;
 
 		}else if(val=='/'){
 
 			operator='/';
 			operand1=parseFloat(displaytext.textContent);
+			current.innerText = current.textContent + "" + val;
 			display.innerText =" " ;
 
 		}else if(val=='%'){
@@ -52,17 +64,76 @@ for (var i = 0; i<buttons.length ; i++) {
 			operator='%';
 			operand1=parseFloat(displaytext.textContent);
 			var result=operand1/100 ;
+			current.innerText = current.textContent/100;
 			displaytext.innerText=result;
 
 		}else if(val=='+/-'){
-			
-			display.innerText = -(display.innerText);  
+
+			display.innerText = -(display.innerText); 
+			current.innerText = -(current.textContent) ; 
 
 		}else{
-
 			display.innerText = +(display.innerText)+ val ;
+			current.innerText = current.textContent + "" + val;
 		}
-
 
 	});
 }
+
+// document.addEventListener('keypress' , function (event){
+// 	var key= event.keyCode ;
+	
+// 	if(key==67){
+// 		display.innerText ="0";
+// 		current.innerText=" ";
+// 	}
+// 	else if(key==13){
+
+// 		operand2=parseFloat(displaytext.textContent) ;
+// 		if(operator =='/' && operand2 == '0')
+//        {
+//           var result = "error";
+//        }else{
+// 		result=eval(operand1 + " " + operator + "" + operand2) ;
+//        }
+// 		displaytext.innerText=result;
+
+// 	}else if(key==43){
+
+// 		operator='+';
+// 		operand1=parseFloat(displaytext.textContent);
+// 		display.innerText =" " ;
+
+// 	}else if(key==45){
+
+// 		operator='-';
+// 		operand1=parseFloat(displaytext.textContent);
+// 		display.innerText =" " ;
+
+// 	}else if(key==42){
+// 		operator='*';
+// 		operand1=parseFloat(displaytext.textContent);
+// 		display.innerText =" " ;
+
+// 	}else if(key==47){
+
+// 		operator='/';
+// 		operand1=parseFloat(displaytext.textContent);
+// 		display.innerText =" " ;
+
+// 	}else if(key==37){
+
+// 		operator='%';
+// 		operand1=parseFloat(displaytext.textContent);
+// 		var result=operand1/100 ;
+// 		displaytext.innerText=result;
+
+// 	}else if(key >=48 && key<=57){
+
+// 		display.innerText += (key-48) ;
+// 	}else{
+// 		display.innerText="error";
+// 	}
+
+
+// });
